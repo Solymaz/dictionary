2dbe002a6d1a01bf96f8c79ccb35346eff0ff2d8
Solmaz <solmaz.mousavie@gmail.com>
2021-05-18 14:18:13 +0200

    added form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary"></div>;
+  function search(event) {
+    event.preventDefault();
+    alert("hi");
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} />
+      </form>
+    </div>
+  );
 }
