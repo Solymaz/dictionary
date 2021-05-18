72667afbbf04139d8a5c011b5a870b28030fa521
Solmaz <solmaz.mousavie@gmail.com>
2021-05-18 14:09:09 +0200

    created dictionary component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <Dictionary />
+        <footer className="App-footer">
+          <small>
+            <a href="https://github.com/Solymaz/dictionary" target="_blank">
+              Open-source code
+            </a>{" "}
+            by{" "}
+            <a
+              href="https://www.linkedin.com/in/solmaz-seyedmousavi/"
+              target="_blank"
+            >
+              Solmaz
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
